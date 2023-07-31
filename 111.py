e9e59922e3484839c550a7b6a925e8df02e9d445
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-31 11:30:42 +0200

    croped 3 images

--- 111.py
+++ 111.py
@@ -3,4 +3,11 @@ from PIL import Image
 image = "IMG_3480.JPG"
 
 img = Image.open(image)
-img.show()
+
+box = (100, 0, 1000, 1000)
+
+img2 = img.crop(box)
+
+img2.save('myimage_cropped3.jpg')
+
+img2.show()
